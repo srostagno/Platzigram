5c2ffa0d8676c235e912949ce3b7de7b24b6d99b
Carlos Sanchez <carlos.sanchez@trabajando.com>
2016-05-31 12:53:36 -0400

    fix languages

--- src/app.js
+++ src/app.js
@@ -21,20 +21,31 @@
 
 	// Traducciónes
 	.config(function($translateProvider) {
-		$translateProvider.translations('es', {
+		$translateProvider
+		.translations('es', {
 			bienvenida: "Bienvenido a Platzigram",
 			boton: "Soy un botón",
 			idiomas: "Idiomas",
 			espanol: "Español",
-			ingles: "Inglés"
+			ingles: "Inglés",
+			japones: "Japonés"
 		})
 		.translations('en', {
 			bienvenida: "Welcome to Platzigram",
 		    boton: "I'm a button",
 		    idiomas: "Languages",
 		    espanol: "Spanish",
-		    ingles: "English"
-		});
+		    ingles: "English",
+		    japones: "Japanese"
+		})
+		.translations('jp', {
+			bienvenida: "Platzigramへようこそ",
+			boton: "私はボタンです",
+			idiomas: "言語",
+			espanol: "スペイン語",
+			ingles: "英語",
+			japones: "日本語"
+		})
 		$translateProvider.preferredLanguage('es');
 	});
 
